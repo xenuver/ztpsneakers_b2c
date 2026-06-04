4abf3e5b39f3045efeff391b39f1b44bcf38c645
xenuver <auikwok2@gmail.com>
2026-06-05 01:25:00 +0700

    feat: buat 7 halaman link footer

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from core.views import index, mark_notifications_read, all_notifications, notification_count
 
 app_name = "core"
@@ -8,4 +9,11 @@ urlpatterns = [
     path("notifications/", all_notifications, name="all_notifications"),
     path("notifications/read/", mark_notifications_read, name="mark_notifications_read"),
     path("notifications/count/", notification_count, name="notification_count"),
+    
+    # Static pages
+    path("faq/", TemplateView.as_view(template_name="pages/faq.html"), name="faq"),
+    path("privacy-policy/", TemplateView.as_view(template_name="pages/privacy.html"), name="privacy"),
+    path("return-policy/", TemplateView.as_view(template_name="pages/return_policy.html"), name="return_policy"),
+    path("about-us/", TemplateView.as_view(template_name="pages/about.html"), name="about"),
+    path("contact-us/", TemplateView.as_view(template_name="pages/contact.html"), name="contact"),
 ]
