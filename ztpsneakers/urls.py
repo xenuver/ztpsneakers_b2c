1e7e80ff9b6fb6a4e7e12417753a97a818f9ab98
xenuver <auikwok2@gmail.com>
2026-06-26 00:04:04 +0700

    fix: serve media files in production
    Django and WhiteNoise do not serve media files by default when DEBUG=False.
    Added django.views.static.serve routing for /media/ in production.

--- ztpsneakers/urls.py
+++ ztpsneakers/urls.py
@@ -34,6 +34,14 @@ urlpatterns = [
     path("admintoko/", include("admintoko.urls")),
 ]
 
+from django.urls import re_path
+from django.views.static import serve
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    # Serve media files in production (Gunicorn) since WhiteNoise only handles static files
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
